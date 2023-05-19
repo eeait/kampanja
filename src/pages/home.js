0231467c409df71930bfab482d400d9aeaa619dd
Eemil Aittokoski <86990190+eeait@users.noreply.github.com>
2023-05-19 13:54:42 +0300

    Nitan lisäyksiä home-sivulle

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,37 @@
+import "./home.css"
+
 const Home = () => {
-  return <h1>Home</h1>
+  return (
+    <div className="home">
+      <h1 className="title">
+        <span className="font">MIETI, MITÄ POSTAAT SOMEEN</span>
+      </h1>
+
+      <h2>
+        <span className="font2">
+          - Kampanja informoimaan oman yksityisyyden vaarantamisesta
+          omien kuvien jakamisella.
+        </span>
+      </h2>
+
+      <div className="blue box">
+        <p>
+          <span className="font3">
+            Tiedätkö mitä selfiesi taustalla oikeasti näkyy?
+          </span>
+        </p>
+        <p>
+          <span className="font2">
+            Sosiaaliseen mediaan julkaisemiseen liittyy paljon
+            riskejä, jos julkaistavaa sisältöä ei pahemmin mieti.
+            Usein mm. selfien taustaakin kannattaa vilkaista, ettei
+            siellä näy yksityisyyttä vaarantavia asioita, kuten ajo-
+            tai pankkikorttia, tai kodin osoitetta.
+          </span>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Home
